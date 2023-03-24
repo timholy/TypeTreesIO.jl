247f63376b88045d5cd8a29b1dab259d67421fe1
Documenter.jl <documenter@juliadocs.github.io>
2023-03-24 22:11:28 +0000

    build based on ad5c244

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TypeTreesIO","category":"page"},{"location":"#TypeTreesIO","page":"Home","title":"TypeTreesIO","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TypeTreesIO.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TypeTreesIO]","category":"page"}]
+}
