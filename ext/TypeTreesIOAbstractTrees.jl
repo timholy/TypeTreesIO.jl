b688ecbd3af853d3b43a972cde7a9479f080c3f2
Tim Holy <tim.holy@gmail.com>
2023-04-03 19:06:35 -0500

    fixes

--- ext/TypeTreesIOAbstractTrees.jl
+++ ext/TypeTreesIOAbstractTrees.jl
@@ -3,13 +3,20 @@ module TypeTreesIOAbstractTrees
 using TypeTreesIO, AbstractTrees
 
 AbstractTrees.children(node::TypeTreesIO.TypeTreeNode) = (v = node.children; v === nothing ? [] : v)
-AbstractTrees.children(node::TypeTreesIO.TypeVarTree) = vcat(children(node.lb), children(node.ub))
+function AbstractTrees.children(node::TypeTreesIO.TypeVarTree)
+    if node.lb === nothing
+        node.ub === nothing && return []
+        return children(node.ub)
+    end
+    node.ub === nothing && return children(node.lb)
+    return vcat(children(node.lb), children(node.ub))
+end
 AbstractTrees.children(node::TypeTreesIO.UnionAllTree) = vcat(children(node.body), children(node.var))
 
 AbstractTrees.nodevalue(node::TypeTreesIO.TypeTreeNode) = node.name
-AbstractTrees.nodevalue(node::TypeTreesIO.UnionAllTree) = string(nodevalue(node.body), " where ", nodevalue(node.var))
+AbstractTrees.nodevalue(node::TypeTreesIO.UnionAllTree) = string(nodevalue(node.body), " where ") #, nodevalue(node.var))
 function AbstractTrees.nodevalue(node::TypeTreesIO.TypeVarTree)
-    body = nodevalue(node.body)
+    body = string(node.tv.name)
     if node.lb === nothing
         node.ub === nothing && return body
         return string(body, "<:", nodevalue(node.ub))
@@ -17,7 +24,5 @@ function AbstractTrees.nodevalue(node::TypeTreesIO.TypeVarTree)
     node.ub === nothing && return string(body, ">:", nodevalue(node.lb))
     return string(nodevalue(node.lb), "<:", body, "<:", nodevalue(node.ub))
 end
-
-__init__() = @info "loaded TypeTreesIOAbstractTrees"
 
 end # module TypeTreesIOAbstractTrees
